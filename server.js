c715d9d0922b853c8b3e4721092b7aef49142f50
atcanbolat <kangalcanbolat@gmail.com>
2020-02-25 23:40:59 -0800

    completed range route and workout:id route

--- server.js
+++ server.js
@@ -16,14 +16,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/workout",
-  { useNewUrlParser: true }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true
+});
 
+//require("./seeders/seed");
 ///////////// HTML ROUTES /////////////
 app.get("/stats", (req, res) => {
-  res.redirect("./stats.html");
+  res.sendFile(path.join(__dirname, "/public/stats.html"));
 });
 
 app.get("/exercise", (req, res) => {
@@ -34,36 +34,54 @@ app.get("/exercise", (req, res) => {
 
 ///////////// ROUTE FOR GETTING ALL WORKOUTS /////////////
 app.get("/api/workouts/", (req, res) => {
-    db.Workout.find({})
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-
-///////////// ROUTE FOR CREATING A NEW WORKOUT /////////////
-app.post("/api/workouts/", (req, res) => {
-  db.Workout.create()
+  db.Workout.find({})
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
     .catch(err => {
-      console.log(err); 
+      console.log(err);
     });
 });
 
-///////////// ROUTE FOR LOGGING A WORKOUT /////////////
-// app.put("/api/workouts/:id", ({body}, res) => {
-//     db.Workout.
-// });
+///////////// ROUTE FOR CREATING A NEW WORKOUT /////////////
+app.post("/api/workouts/", (req, res) => {
+  db.Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+/////////// ROUTE FOR LOGGING A WORKOUT /////////////
+app.put("/api/workouts/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  console.log(id);
+  console.log(body);
+  db.Workout.findOneAndUpdate({ _id: id }, { $push: { exercises: body } })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 ///////////// ROUTE FOR WORKOUT RANGE /////////////
-// app.get("/api/workouts/range", (req, res) => {
-//   db.Workout.
-// })
-
+app.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({}, (err, data) => {
+    console.log("/workuts/range is hit");
+    console.log(data);
+  })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
